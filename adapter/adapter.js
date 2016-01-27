fe73ee990dbb4754aa2a70ea75095d5a24b7cfd9
windsolider <windsolider@163.com>
2016-01-27 09:31:37 +0800

    data adapter

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -25,3 +25,29 @@ renderMap(mapType['googleMap']);
 renderMap(mapType['baiduMap']);
 
 
+//data adapter
+var getGuangdongCity = function() {
+	var guangdongCity = [
+			{name: 'shenzhen', id: 11},
+			{name: 'guangzhow', id: 12}
+		];
+	return guangdongCity;
+};
+
+var render = function(fn) {
+	console.log(JSON.stringify(fn()));
+};
+
+var addressAdapter = function(oldAddressfn) {
+	var address = {};
+	var oldAddress = oldAddressfn();
+	for (var i = 0, c; c = oldAddress[i++];) {
+		address[c.name] = c.id;
+	}
+	return function() {
+		return address;
+	}
+};
+
+render(addressAdapter(getGuangdongCity));
+
