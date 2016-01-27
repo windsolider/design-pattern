403619470616b9048b7adc5a2a0accebd488e6f0
windsolider <windsolider@163.com>
2016-01-27 09:21:55 +0800

    adapter others

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -1,49 +1,27 @@
-function Thead() {
-	this.thead = null;
-	this.strategy = null;
-}
-Thead.prototype.setThead = function(thead) {
-	this.thead = thead;
-};
-Thead.prototype.setStrategy = function(strategy) {
-	this.strategy = strategy;
-};
-Thead.prototype.renderThead = function() {
-	return this.strategy.render(this.thead);
-};
-function PayedThead() {
+var googleMap = {
 
-}
-PayedThead.prototype.render = function(thead) {
-	if (typeof thead === 'undefined' || !thead.length) {
-		return ;
+	show: function() {
+		consoe.log('render');
 	}
-	var theadHtml = '<tr><th >产品名称</th>\
-				<th >投资本金（元）</th>\
-				<th >预期年收益率（%）</th>\
-				<th >待收收益（元）</th>\
-				<th >已经计息|剩余期限</th>\
-				<th >回款日期（最晚）</th>\
-				<th>操作</th></tr>';
-	thead.html(theadHtml);
 };
-function TransThead() {
-   
-}
-TransThead.prototype.render = function(thead) {
-	if (typeof thead === 'undefined' || !thead.length) {
-		return ;
+var baiduMap = {
+
+	display: function() {
+		console.log('render');
 	}
-	var theadHtml = '<tr><th >产品名称</th>\
-				<th >已转让金额（元）</th>\
-				<th >手续费用（元）</th>\
-				<th >已收到账（元）</th>\
-				<th>操作</th></tr>';
-	thead.html(theadHtml);
+};
+var baiduMapAdapter = {
+
+	show: function() {
+		return baiduMap.display();
+	}
 };
 
-var thead = new Thead();
-thead.setThead($('.regular-table thead'));
-thead.setStrategy(new PayedThead());
-thead.renderThead();
+var mapType = {
+	'googleMap': googleMap,
+	'baiduMap': baiduMapAdapter
+};
+renderMap(mapType['googleMap']);
+renderMap(mapType['baiduMap']);
+
 
